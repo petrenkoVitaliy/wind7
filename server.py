2b3ab9dc1d0854f1d82b13902dae61b27238864f
petrenkoVitaliy <vitaliy.ptt@gmail.com>
2025-06-02 10:17:00 +0300

    update relay

--- server.py
+++ server.py
@@ -46,8 +46,7 @@ rtc_config = RTCConfiguration(
     iceServers=[
         RTCIceServer(urls=["stun:stun.l.google.com:19302"]),
         RTCIceServer(
-            urls=["turn:openrelay.metered.ca:80", "turn:openrelay.metered.ca:443",
-                  "turn:openrelay.metered.ca:443?transport=tcp"],
+            urls=["turn:openrelay.metered.ca:443?transport=tcp"],
             username="openrelayproject",
             credential="openrelayproject"
         )
